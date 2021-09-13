6f692b506b864302cee591d723ef7ac0bf70c5d1
yersy <yaliaga@canvia.com>
2021-09-13 16:45:31 -0500

    Agregar archivos de proyecto.

---
+++ ConsolaAranda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aranda.ASDK.External;
+
+namespace ConsolaAranda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Token token = new Token();
+
+            string token_string = token.Get(0);
+
+            Console.WriteLine("Token: ");
+            Console.WriteLine(token_string);
+            Console.ReadLine();
+        }
+    }
+}
